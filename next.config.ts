fbd25b4a469ce0458c17437f45527e6e662926e0
blendibox <loja@blendibox.com.br>
2026-05-14 18:59:27 -0300

    hhh

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: { unoptimized: true },
-  experimental: {
-    turbo: undefined  // desativa Turbopack
-  }
+  /* config options here */
 };
 
 export default nextConfig;
